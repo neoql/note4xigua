54556adbfc22061bd5ea43a3a652b1b26bc39e9a
neoql <nql1156325827@163.com>
2019-03-20 18:12:51 +0800

    scond commit

--- 3_linear_model/exercise_3_3.py
+++ 3_linear_model/exercise_3_3.py
@@ -5,24 +5,20 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    density = []
-    ratio_sugar = []
-    is_good = []
-
     with open('../dataset/xigua_dataset_3_0_alpha.csv') as fd:
         cr = csv.reader(fd)
-        for i, row in enumerate(cr):
-            if i == 0: continue
-            density += [float(row[1])]
-            ratio_sugar += [float(row[2])]
-            is_good += [row[3] == '是']
+        rows = list(cr)[1:]
 
-    density = np.array(density)
-    ratio_sugar = np.array(ratio_sugar)
-    is_good = np.array(is_good)
+    X = np.zeros([len(rows), 2])
+    y = np.zeros((len(rows),))
 
-    plt.scatter(density[is_good == 1], ratio_sugar[is_good == 1], marker='+', color='red')
-    plt.scatter(density[is_good == 0], ratio_sugar[is_good == 0], marker='+', color='black')
+    for i, row in enumerate(rows):
+        X[i, 0] = float(row[1])    # density
+        X[i, 1] = float(row[2])    # radio sugar
+        y[i] = 1 if row[3] == '是' else 0
+
+    plt.scatter(X[y == 1, 0], X[y == 1, 1], marker='+', color='red')
+    plt.scatter(X[y == 0, 0], X[y == 0, 1], marker='+', color='black')
     plt.show()
 
 

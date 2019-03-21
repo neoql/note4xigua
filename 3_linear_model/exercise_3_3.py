1d57bde6dec5debde98322eb57581a152087138a
neoql <nql1156325827@163.com>
2019-03-21 18:57:54 +0800

    完成练习3.3

--- 3_linear_model/exercise_3_3.py
+++ 3_linear_model/exercise_3_3.py
@@ -1,7 +1,31 @@
+#!/usr/bin/env python
+
 import csv
 
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+def p1(x, beta):
+    t = np.exp(np.dot(beta.T, x))
+    return t / (1 + t)
+
+
+def dl(X, y, beta):
+    m = len(X)
+    sum = 0
+    for i in range(m):
+        sum += X[i] * (y[i] - p1(X[i], beta))
+    return -sum
+
+
+def d2l(X, y, beta):
+    m = len(X)
+    sum = 0
+    for i in range(m):
+        x_i = X[i].reshape((3, 1))
+        sum += np.dot(x_i, x_i.T) * p1(X[i], beta) * (1 - p1(X[i], beta))
+    return sum
 
 
 def main():
@@ -13,12 +37,41 @@ def main():
     y = np.zeros((len(rows),))
 
     for i, row in enumerate(rows):
-        X[i, 0] = float(row[1])    # density
-        X[i, 1] = float(row[2])    # radio sugar
-        y[i] = 1 if row[3] == '是' else 0
+        X[i, 0] = float(row[1])  # density
+        X[i, 1] = float(row[2])  # radio sugar
+        y[i] = row[3] == '是'
 
-    plt.scatter(X[y == 1, 0], X[y == 1, 1], marker='+', color='red')
-    plt.scatter(X[y == 0, 0], X[y == 0, 1], marker='+', color='black')
+    old_X = X
+
+    d = X.shape[1]  # number of attributes
+    epsilon = 0.001  # error range
+
+    beta = np.random.rand(d + 1)
+    X = np.concatenate((X, np.ones((len(X), 1))), axis=1)
+
+    i = 0
+    while True:
+        i += 1
+        g = dl(X, y, beta)
+        if np.linalg.norm(g) < epsilon:
+            break
+        G = d2l(X, y, beta)
+        beta = beta - np.dot(np.linalg.inv(G), g)
+
+    X = old_X
+    beta = beta.reshape(-1)
+
+    x = np.arange(0, 0.9, 0.01)
+    f = lambda x: (-beta[2] - x * beta[0]) / beta[1]
+
+    plt.title('Logistic Regression')
+    plt.xlabel('Density')
+    plt.ylabel('Sugar-containing Rate')
+    plt.scatter(X[y == 1, 0], X[y == 1, 1], marker='+', color='red', s=80, label='Good Watermelon')
+    plt.scatter(X[y == 0, 0], X[y == 0, 1], marker='+', color='black', s=80, label='Bad Watermelon')
+    plt.plot(x, f(x))
+    plt.legend(loc='upper right')
+
     plt.show()
 
 

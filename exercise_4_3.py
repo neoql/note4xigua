f5fd3ec8c0244520bff621682c2855f594f5dfd3
neoql <nql1156325827@163.com>
2019-04-10 14:46:18 +0800

    exersise 4.3

---
+++ exercise_4_3.py
@@ -0,0 +1,26 @@
+class DecisionTree(object):
+    def tree_generate(self):
+        pass
+
+
+class Node(object):
+    pass
+
+
+def use_id3():
+    """ID3"""
+    pass
+
+
+def use_c4dot5():
+    """C4.5"""
+    pass
+
+
+def main():
+    use_id3()
+    use_c4dot5()
+
+
+if __name__ == '__main__':
+    main()
